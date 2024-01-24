410724dbac189071af705ea83e021f4bd33cf998
aatituanav <alvaro.andres1996@hotmail.com>
2024-01-24 09:47:02 -0500

    Estructura Inicial

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Index from './src';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Puto el que lee!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Index/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
